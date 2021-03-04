33e7de7c58d1c545bf450adb74ba91f6d8eca215
Fedor Dudinskiy <fdudinsk@redhat.com>
2021-03-04 14:28:06 +0100

    -Unused code

--- src/test/java/ReproducerTest.java
+++ src/test/java/ReproducerTest.java
@@ -1,9 +1,7 @@
-//import io.vertx.db2client.DB2Pool;
 
 import graphql.schema.idl.SchemaParser;
 import graphql.schema.idl.TypeDefinitionRegistry;
 import io.vertx.junit5.VertxExtension;
-import io.vertx.junit5.VertxTestContext;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
