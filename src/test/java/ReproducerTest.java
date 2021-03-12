acff352886bc8f7ccc7913f2063583b11c807ddb
Jose Carranza <jcarranz@redhat.com>
2021-03-12 12:46:47 +0100

    -unused import

--- src/test/java/ReproducerTest.java
+++ src/test/java/ReproducerTest.java
@@ -9,7 +9,6 @@ import io.vertx.servicediscovery.Record;
 import io.vertx.servicediscovery.ServiceDiscovery;
 import io.vertx.servicediscovery.ServiceDiscoveryOptions;
 import io.vertx.servicediscovery.types.HttpEndpoint;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
